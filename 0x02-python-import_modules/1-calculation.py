6b0e85756297f8b99e2657654a543fd1af844657
latiah <kimtifah2@gmail.com>
2022-06-07 04:24:34 -0700

    module import

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+import calculator_1
+result1 = calculator_1.add(10, 5)
+print("{:d}".format(result1))
+result2 = calculator_1.sub(10, 5)
+print("{:d}".format(result2))
+result3 = calculator_1.mul(10, 5)
+print("{:d}".format(result3))
+result4 = calculator_1.div(10, 5)
+print("{:d}".format(result))
+
