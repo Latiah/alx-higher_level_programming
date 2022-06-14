980102c03bbc229529cc9a99722425b0fcc4cdf0
Latifah <kimtifah2@gmail.com>
2022-06-15 00:18:42 +0200

    first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+   character = None
+    if sentence:
+        character = sentence[0]
+    length = len(sentence)
+    return (length, character)
